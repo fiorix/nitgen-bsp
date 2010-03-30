d4d22789501c892ad17191a0c30e43478ff85476
Alexandre Fiori <alef@ubuntu.(none)>
2010-03-30 09:48:09 -0700

    updated project url

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     version="0.1",
     author="Alexandre Fiori",
     author_email="fiorix@gmail.com",
-    url="http://github.com/fiorix/nitgen_bsp",
+    url="http://github.com/fiorix/nitgen-bsp",
     packages=["NitgenBSP"],
     ext_modules=[
-        Extension("NitgenBSP/_bsp_module", [
-            "NitgenBSP/bsp_module.c",
+        Extension("NitgenBSP/_bsp_core", [
+            "NitgenBSP/bsp_core.c",
         ],
         include_dirs=["/usr/local/NITGEN/eNBSP/include"],
         libraries=["pthread", "NBioBSP"], )
