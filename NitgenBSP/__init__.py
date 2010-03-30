d37d90550d48a718f449e99f3b8ae524ea1cf13c
Alexandre Fiori <alef@ubuntu.(none)>
2010-03-30 05:03:34 -0700

    added documentation

--- NitgenBSP/__init__.py
+++ NitgenBSP/__init__.py
@@ -4,10 +4,20 @@
 from PIL import Image
 from NitgenBSP import _bsp_module
 
+"""Nitgen fingerprint recognition API"""
+
 class FingerText(str):
+    """The Text-Encoded version of a fingerprint's FIR.
+    """
     pass
 
 class Finger(object):
+    """The Finger class provides the FIR Handle, raw
+    image buffer provided by the device, as well as
+    image dimentions (width and height).
+    It may also convert the raw buffer to a PIL image,
+    as well as the Text-Encoded FIR.
+    """
     def __init__(self, handler, data, width, height):
         self.__handler = handler
         self.buffer, self.FIR = data
@@ -15,9 +25,16 @@ class Finger(object):
         self.height = height
 
     def text(self):
+        """Return the Text-Encoded version of the FIR for this
+        fingerprint, which may be used to store the fingerprint
+        in a database as well as later verification.
+        """
         return FingerText(_bsp_module.text_fir(self.__handler, self.FIR))
 
     def image(self):
+        """Return the PIL image of this fingerprint, which may be
+        used to be saved on disk as JPG, PNG, etc.
+        """
         return Image.fromstring("L",
             (self.width, self.height), self.buffer)
 
@@ -26,18 +43,30 @@ class Finger(object):
  
 
 class Handler:
+    """A Nitgen device handler
+    """
     def __init__(self):
+        """Initialize the Nitgen API and open the first available device
+        using the auto-detection mode.
+        """
         self.__bsp = _bsp_module.open()
         self.__handler = self.__bsp[0]
         self.__image_width = self.__bsp[1]
         self.__image_height = self.__bsp[2]
 
     def capture(self):
+        """Capture a fingerprint from the device and return an instance
+        of the Finger class.
+        """
         return Finger(self.__handler,
             _bsp_module.capture(*self.__bsp),
             self.__image_width, self.__image_height)
 
     def verify(self, cap1, cap2):
+        """Perform verification of two fingerprints by receiving either
+        a pair of Finger or FingerText instances.
+        Returns 0 or 1, depending on finger verification.
+        """
         if isinstance(cap1, Finger):
             cap1 = cap1.FIR
         elif isinstance(cap1, FingerText):
@@ -58,4 +87,6 @@ class Handler:
         return _bsp_module.verify(self.__handler, cap1, cap2)
 
     def close(self):
+        """Close the currently opened Nitgen device
+        """
         _bsp_module.close(self.__handler)
---
+++ README.rst
@@ -0,0 +1,53 @@
+=========
+NitgenBSP
+=========
+:Info: See `github <http://github.com/fiorix/nitgen-bsp>`_ for the latest source.
+:Author: Alexandre Fiori <fiorix@gmail.com>
+
+About
+=====
+
+NitgenBSP is a Python extentension based on the `Nitgen SDK for Linux <http://www.nitgen.com/eng/product/enbsp_sdk.html>`_. It currently supports Nitgen fingerprint recognition devices such as `Fingkey Hamster <http://www.nitgen.com/eng/product/finkey.html>`_ and `Fingkey Hamster II <http://www.nitgen.com/eng/product/finkey2.html>`_.
+
+Implementation details
+----------------------
+
+- It has been tested under Ubuntu Linux 9.10
+- Require root access level (actually depends on file permission of /dev/nitgen0)
+- Only supports the auto-detection mode (I don't have 2 devices do try manual selection)
+- Supports verification with the FIR Handle and Text-Encoded FIR (not the FULL FIR)
+- Allows the Text-Encoded FIR to be saved on remote database for later verification
+- Text-Encoded FIR does not allow multi-byte encoding
+- Ships with `PIL <http://www.pythonware.com/products/pil/>`_ support and allows saving fingerprint images as JPG, PNG, etc
+
+Documentation and Examples
+==========================
+
+The source code ships with built-in Python Docstring documentation for class reference. It also ships with examples in the `examples/ <http://github.com/fiorix/nitgen-bsp/tree/master/examples/>`_ subdirectory.
+
+However, using NitgenBSP is pretty straightforward even for those with no experience with biometric devices.
+Here is an example of simple usage::
+
+ #!/usr/bin/env python
+ # coding: utf-8
+ 
+ import NitgenBSP
+
+ if __name__ == "__main__":
+    handler = NitgenBSP.Handler()
+
+    finger = handler.capture()
+    image = finger.image()
+    image.save("out.png")
+
+    print "your fingerprint text-encoded FIR is:", finger.text()
+
+In the near future I'll provide support for the SDK's Search Engine API, and probably other features such as manual device selection. See the `TODO <http://github.com/fiorix/nitgen-bsp/tree/master/TODO>`_ for details.
+
+Credits
+=======
+Thanks to (in no particular order):
+
+- Nitgen Brazil
+  
+  - For providing documentation and allowing this code do be published
